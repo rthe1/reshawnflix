b66cc7d9f82bc8cbbacb6bba64166b2422e0a474
Reshawn Hamilton <reshawnhamilton97@gmail.com>
2022-05-10 16:55:58 -0400

    navbar finished

--- reshawnflix/src/App.js
+++ reshawnflix/src/App.js
@@ -2,25 +2,25 @@ import React from 'react';
 import './App.css';
 import Row from './Row';
 import requests from './requests';
-import Banner from './Banner.js'
+import Banner from './Banner'
+import Nav from './Nav'
 
 function App() {
   return (
-    <div className="App">
-
-
-     <Banner/>
-     <Row 
-     title="Netflix Originals" 
-     fetchURL={requests.fetchNetflixOriginals}
-     isLargeRow
-     />
-     <Row title="Trending Now" fetchURL={requests.fetchTrending}/>
-     <Row title="Action Movies" fetchURL={requests.fetchActionMovies}/>
-     <Row title="Comedy Movies" fetchURL={requests.fetchComedyMovies}/>
-     <Row title="Horror Movies" fetchURL={requests.fetchHorrorMovies}/>
-     <Row title="Romance Movies" fetchURL={requests.fetchRomanceMovies}/>
-     <Row title="Documentaries" fetchURL={requests.fetchDocuemtariesMovies}/>
+    <div className="app">
+      <Nav />
+      <Banner />
+      <Row
+        title="Netflix Originals"
+        fetchURL={requests.fetchNetflixOriginals}
+        isLargeRow
+      />
+      <Row title="Trending Now" fetchURL={requests.fetchTrending} />
+      <Row title="Action Movies" fetchURL={requests.fetchActionMovies} />
+      <Row title="Comedy Movies" fetchURL={requests.fetchComedyMovies} />
+      <Row title="Horror Movies" fetchURL={requests.fetchHorrorMovies} />
+      <Row title="Romance Movies" fetchURL={requests.fetchRomanceMovies} />
+      <Row title="Documentaries" fetchURL={requests.fetchDocuemtariesMovies} />
     </div>
   );
 }
--- reshawnflix/src/Banner.js
+++ reshawnflix/src/Banner.js
@@ -1,7 +1,11 @@
 import React, { useEffect, useState } from 'react';
 import axios from './axios';
 import requests from './requests'
+import './Banner.css'
 
+function truncate(str, n) {
+  return str?.length > n ? str.substr(0, n - 1) + "..." : str;
+}
 
 function Banner() {
 
@@ -14,28 +18,38 @@ function Banner() {
       setMovie(request.data.results[
         Math.floor(Math.random() * request.data.results.length)
       ]
-     )
+      )
     }
     fetchData();
   }, [])
 
   return (
-    <header className="banner">
+    <header
+      className="banner"
+      style={{
+        backgroundSize: "cover",
+        backgroundImage: `url(
+        "https://image.tmdb.org/t/p/original/${movie?.backdrop_path}"
+        )`,
+        backgroundPosition: "center center",
+
+      }}>
       <div className="banner_contents">
 
-      <h1 className="banner_title" >{movie?.title || movie?.name || movie?.original}</h1>
-      
-       <button className="banner_button">
-         Play
-         </button> 
-       <button className="banner_button">
-         My List
-         </button>    
-      </div>
+        <h1 className="banner_title" >{movie?.title || movie?.name || movie?.original}</h1>
 
-      <h1 className="banner_description">
-    {movie?.overview}
-      </h1> 
+        <button className="banner_button">
+          Play
+        </button>
+        <button className="banner_button">
+          My List
+        </button>
+
+        <h1 className="banner_description">
+          {truncate(movie?.overview, 150)}
+        </h1>
+      </div>
+        <div className="banner_fadebottom"/>
     </header>
   )
 }
